f2f8d5871901650f913ad9787ec84666210ca463
Nang Eaindra Hein <eaindraann@gmail.com>
2025-06-28 01:14:22 +0800

    Final fix: Single HashRouter in index.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./LandingPage";
-import ProductListing from "./ProductListing";
-import ShoppingCart from "./ShoppingCart";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './LandingPage';
+import ProductListing from './ProductListing';
+import ShoppingCart from './ShoppingCart';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/products" element={<ProductListing />} />
-        <Route path="/cart" element={<ShoppingCart />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/products" element={<ProductListing />} />
+      <Route path="/cart" element={<ShoppingCart />} />
+    </Routes>
   );
 }
 
